4c5cf2704dec733186650e3c1fb96a705cbc3303
Karim DRIDI <karim.dridi@gmail.com>
2021-01-21 21:32:45 +0100

    IXWebSocket install script

---
+++ scripts/03-IXWebSocket.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+cd $(dirname $0)/..
+
+rm -rf tmp
+mkdir tmp
+cd tmp
+
+cmake ../vendor/github.com/machinezone/IXWebSocket.git \
+	-DCMAKE_BUILD_TYPE=Debug \
+	-DCMAKE_INSTALL_PREFIX=$(pwd)/../system \
+	-DUSE_TLS=ON \
+	-DUSE_ZLIB=ON
+
+make VERBOSE=1 -j8
+make install
+
+cd -
+rm -rf tmp
